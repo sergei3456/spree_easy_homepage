d8b474619f07dd051f75381bd3edbfb906572988
Сергей Сечин <sechins01@yandex.ru>
2020-11-07 13:33:50 +0300

    v 1.0.158 Замена иконок

--- app/models/spree/home_section_product.rb
+++ app/models/spree/home_section_product.rb
@@ -3,6 +3,11 @@ module Spree
     acts_as_list scope: :home_section
 
     belongs_to :home_section, class_name: 'Spree::HomeSection', required: true
-    belongs_to :product, class_name: 'Spree::Product', required: true
+    belongs_to :product,
+               -> { includes :reviews },
+               class_name: 'Spree::Product',
+               required: true,
+               counter_cache: true,
+               inverse_of: :reviews
   end
 end
